d504e29b17ef694231c21724da7658d2667d68c5
nicmackenzie <nicmackenzie@live.com>
2023-06-05 14:59:03 +0300

    fetch users

--- src/context/app-context.jsx
+++ src/context/app-context.jsx
@@ -4,15 +4,20 @@ const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
   const [jobListings, setJobListings] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8001/jobs')
       .then(rs => rs.json())
       .then(jobs => setJobListings(jobs));
+
+    fetch('http://localhost:8001/users')
+      .then(rs => rs.json())
+      .then(users => setUsers(users));
   }, []);
 
   return (
-    <AppContext.Provider value={{ jobListings }}>
+    <AppContext.Provider value={{ jobListings, users }}>
       {children}
     </AppContext.Provider>
   );

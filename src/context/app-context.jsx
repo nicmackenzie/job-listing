d5494081ba53cecaf0b146a8a311ccb0341c5916
nicmackenzie <nicmackenzie@live.com>
2023-06-05 16:34:00 +0300

    add user detail state and changing its state. Provide userdetails value to provider

--- src/context/app-context.jsx
+++ src/context/app-context.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect, createContext } from 'react';
 
-const AppContext = createContext();
+const AppContext = createContext({
+  jobListings: [],
+  users: [],
+  userDetails: {},
+  onLogin: () => {},
+});
 
 const AppProvider = ({ children }) => {
   const [jobListings, setJobListings] = useState([]);
   const [users, setUsers] = useState([]);
+  const [userDetails, setUserDetails] = useState();
 
   useEffect(() => {
     fetch('http://localhost:8001/jobs')
@@ -16,8 +22,12 @@ const AppProvider = ({ children }) => {
       .then(users => setUsers(users));
   }, []);
 
+  const onLogin = user => {
+    setUserDetails(user);
+  };
+
   return (
-    <AppContext.Provider value={{ jobListings, users }}>
+    <AppContext.Provider value={{ jobListings, users, userDetails, onLogin }}>
       {children}
     </AppContext.Provider>
   );

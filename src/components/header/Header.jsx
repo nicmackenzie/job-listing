4f67d284770d6de3d8bfeba800a661e3a78f238d
nurdinhajo <nurudiin222@gmail.com>
2023-06-06 22:19:49 +0300

    feat - User Authentication btns

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,31 @@
-      //NavBar
+      // ?NavBar
 // Header Component - <Header />
 // Will be used to display our navigation. **
 
 
-      // *Log In & out
-// ?Will receive isLoggedIn(boolean) and filters(array) props from <App />
+      // ?Log In & out
+// Will receive isLoggedIn(boolean) and filters(array) props from <App />
 // Will render the navigation based on user login status as below
 // If user not logged in, have two links. One for log in and the other for sign up.
 // If user is logged in and as employer, have three links. One for adding new job, the other for my jobs-to display jobs posted for that company, and the last link for logout.
 // If user is logged in and as user, have two links. One for my bookmarked jobs and the other for logout.
 
-
+        // ?Filter
 // Will have one child component - <Filters />
 // Will use condition rendering to  display the <Filters /> component based on if the filters prop, which is an array, has any elements, i.e if filters.length > 0. We are doing this so that the <Filter /> component is only displayed when filters are applied.
 
 
 
 
-import React from 'react'
-import Navbar from '../navbar/Navbar'
+import Navbar from '../navbar/Navbar';
 
-function Header() {
+function Header({ user, filters }) {
   return (
-    <header>
-      <Navbar/>
+    <header className="-full h-36 bg-primary p-4">
+      <Navbar user={user} />
+      {/* after this we will render the search and filters component once they finish working on them */}
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,38 @@
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ user }) {
   return (
     <nav className="flex justify-end">
-     
+      <ul className="flex gap-4 items-center">
+        {!user && (
+          <>
+            <li>
+              <Link
+                to="/login"
+                className="text-white font-semibold text-lg transition-all hover:text-primary-700"
+              >
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/login"
+                className="text-white font-semibold text-lg transition-all hover:text-primary-700"
+              >
+                Register
+              </Link>
+            </li>
+          </>
+        )}
+        {user && user.role === 'employer' && (
+          <Link
+            className="text-white font-semibold text-lg transition-all hover:text-primary-700"
+            to="/jobs/new"
+          >
+            New Job
+          </Link>
+        )}
+      </ul>
     </nav>
   );
 }

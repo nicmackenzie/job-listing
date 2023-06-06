e51297c3e46235f67e3dfaab0ecabdfca6c1048b
Sharon Awuor <sawuor395@gmail.com>
2023-06-06 23:12:57 +0300

    map method added

--- src/components/ui/Filter.jsx
+++ src/components/ui/Filter.jsx
@@ -4,7 +4,10 @@ import React from 'react'
 function Filter({ filter }) {
   return (
     <div>
-      
+      <h3>Filters</h3>
+      {filter.map((filter, id) => {
+        <Filter key={id} filter={filter} />
+      })}
     </div>
   )
 }

974d219d0b9751759933f2d3f8c397b914e37bab
nicmackenzie <nicmackenzie@live.com>
2023-06-07 21:58:16 +0300

    add styles for listings parent div

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,16 +1,16 @@
-import React,{useContext} from 'react'
-import Listing from './Listing'
-import { AppContext } from '../context/app-context'
+import { useContext } from 'react';
+import Listing from './Listing';
+import { AppContext } from '../context/app-context';
 
 function JobListing() {
-   const {jobListings}= useContext(AppContext)
+  const { jobListings } = useContext(AppContext);
   return (
-    <div>
-      {jobListings.map((job)=>{
-        return <Listing key={job.id} job={job}/>
+    <div className="flex flex-col gap-4 mt-4">
+      {jobListings.map(job => {
+        return <Listing key={job.id} job={job} />;
       })}
     </div>
-  )
+  );
 }
 
-export default JobListing
+export default JobListing;

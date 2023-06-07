6620bc166d2df66bc20b6fd3a949a3115d9d6efb
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-06-07 12:04:12 +0300

    add event listeners

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,21 +1,24 @@
-import React,{useContext} from 'react'
-import { AppContext } from '../context/app-context'
-function Listing({job}) {
-  const {onAddFilter}=useContext(AppContext)
-  function handleClick(e){
-    onAddFilter(e.target.textContent)
-  }
+import { useContext } from 'react';
+import { AppContext } from '../context/app-context';
+
+function Listing({ job }) {
+  const { onAddFilter } = useContext(AppContext);
+  const clickHandler = e => {
+    onAddFilter(e.target.textContent);
+  };
   return (
     <div className="card">
       <div className="flex gap-4 items-center">
-<img src={job.logo} alt="company logo" className="w-14 h-14 rounded-full"/>
-  <div className="flex flex-col gap-1 mt-5 lg:mt-0">
-    <div className="flex gap-1 items-center">
-  <p className="text-primary text-[12px]">{job.company}</p>
-  </div>
-      <div>
-      <p className="text-primary text-[15px] font-bold">{job.position}</p>
-      </div>
+        <img
+          src={job.logo}
+          alt="company logo"
+          className="w-14 h-14 rounded-full"
+        />
+        <div className="flex flex-col gap-1 mt-5 lg:mt-0">
+          <div className="flex gap-1 items-center">
+            <p className="text-primary text-[12px]">{job.company}</p>
+          </div>
+          <p className="text-primary text-[15px] font-bold">{job.position}</p>
           <div className="flex items-center gap-4 text-[12px] text-primary-600">
             <div>{job.postedAt}</div>
             <div>{job.contract}</div>
@@ -23,15 +26,26 @@ function Listing({job}) {
           </div>
         </div>
       </div>
-              <div className="flex gap-2 items-center flex-wrap" >
-                <span onClick={handleClick} className="filter-pills role">{job.role}</span>
-                <span onClick={handleClick} className="filter-pills level">{job.level}</span>
-                {job.languages.map((language)=>{
-                  return <span onClick={handleClick} className="filter-pills language" key={language}>{language}</span>
-                })}
-              </div>
+      <div className="flex gap-2 items-center flex-wrap">
+        <span className="filter-pills role" onClick={clickHandler}>
+          {job.role}
+        </span>
+        <span className="filter-pills level" onClick={clickHandler}>
+          {job.level}
+        </span>
+
+        {job.languages.map(language => (
+          <span
+            className="filter-pills language"
+            key={language}
+            onClick={clickHandler}
+          >
+            {language}
+          </span>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Listing
+export default Listing;

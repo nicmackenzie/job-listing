d676c718c53b4fbec7dfaebe1b96d7c9c49801f8
Sharon Awuor <sawuor395@gmail.com>
2023-06-07 19:36:54 +0300

    searchbar

---
+++ src/components/header/SearchBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SearchBar({ searchTerm, onSearch }) {
+  const handleSearchChange = (event) => {
+    onSearch(event.target.value); 
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder="Search filters"
+      value={searchTerm}
+      onChange={handleSearchChange}
+      className="search-input"
+    />
+  );
+}
+
+export default SearchBar;

60b88c2abe34ff8c2f6685f1360f9e4c04a06157
Sharon Awuor <sawuor395@gmail.com>
2023-06-08 10:22:28 +0300

    useEffect added

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -7,3 +7,14 @@ function JobDetails() {
   const [job, setJob] = useState();
   const { jobListings, onLogout, userDetails } = useContext(AppContext);
   const { id } = useParams();
+  
+  useEffect(() => {
+    if (jobListings.length > 0) {
+      const found = jobListings.find(listing => +listing.id === +id);
+      if (!found) {
+        throw new Error('not found');
+      } else {
+        setJob(found);
+      }
+    }
+  }, [id, jobListings]);
